f697b909e831bf678b9fb70848446a7f79c50849
gustavojung23 <gustavojung.dev@gmail.com>
2025-06-18 21:24:18 -0300

    Create file __init__.py

---
+++ app/__init__.py
@@ -0,0 +1,21 @@
+import os
+from db import get_connection
+from flask import Flask, render_template
+##from controllers.instituicoes_controller import institution_bp
+
+
+def create_app():
+    app = Flask(__name__, template_folder='views')
+    app.secret_key = os.getenv('SECRET_KEY')
+    app.config['DATABASE'] = 'app.db'
+
+    with get_connection() as conn:
+        conn.execute('PRAGMA journal_mode=WAL;')
+
+    ##app.register_blueprint(institution_bp)
+
+    @app.route('/')
+    def index():
+        return render_template('index.html')
+
+    return app
