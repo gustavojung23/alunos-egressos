521805bd9c1a0d1bbc8406664b168411a89fdfef
gustavojung23 <gustavojung.dev@gmail.com>
2025-06-18 21:33:32 -0300

    Remove app. init line code

--- app/controllers/instituicoes_controller.py
+++ app/controllers/instituicoes_controller.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, render_template, request, redirect, url_for, flash
-from app.models.instituicoes_model import Institution
-from app.repositories.instituicoes_repository import InstitutionRepository
+from models.instituicoes_model import Institution
+from repositories.instituicoes_repository import InstitutionRepository
 
 # Encapsulamento de rotas - Instituição
 institution_bp = Blueprint('instituicao', __name__, url_prefix='/instituicoes')
