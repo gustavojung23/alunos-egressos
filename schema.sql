f86dfc155a81e51f0a4b5f1885d1ab08115ebbdd
gustavojung23 <gustavojung.dev@gmail.com>
2025-06-19 09:45:27 -0300

    Create table Cursos

--- schema.sql
+++ schema.sql
@@ -1,7 +1,17 @@
 --SQL para criação das tabelas
+/*
+DROP TABLE cursos;
+*/
 
 CREATE TABLE IF NOT EXISTS instituicoes(
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     nome VARCHAR(150) NOT NULL,
     razao_social VARCHAR(150) NOT NULL
-)
+);
+
+CREATE TABLE IF NOT EXISTS cursos(
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    nome_curso VARCHAR(150) NOT NULL,
+    id_instituicao INTEGER NOT NULL,
+    FOREIGN KEY (id_instituicao) REFERENCES instituicoes(id) ON DELETE CASCADE
+);
